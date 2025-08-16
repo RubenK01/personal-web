e9139753946835174ee5825434ae0ea1f63fda0c
Ruben <rbn994@gmail.com>
2025-08-16 12:20:32 +0200

    Some lintr fixes

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -52,8 +52,8 @@ export default function Navigation() {
           >
             <span className="inline-flex items-center space-x-2">
                 <span className="md:hidden">
-                  <span className="font-extrabold text-2xl tracking-tight text-white">Rubén C</span>
-                  <span className="ml-2 xs:inline text-gray-400 text-sm">  DevSecOps & Cloud Engineer</span>
+                  <span className="font-extrabold text-2xl tracking-tight text-white">Rubén C.</span>
+                  <span className="ml-2 text-gray-400 text-sm"> DevSecOps & Cloud Engineer</span>
                 </span>
                 <span className="hidden md:inline font-extrabold text-2xl tracking-tight text-white">Rubén Casado</span>
                 <span className="hidden md:inline text-gray-400 text-base">· DevSecOps & Cloud Engineer</span>
